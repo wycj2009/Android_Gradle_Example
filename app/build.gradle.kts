ff6006138721c5f712acd8b173f54c8038115689
jooan-kim <jooan.kim@kakaocorp.com>
2023-04-05 18:21:45 +0900

    Migrate to version catalogs with 'libs.versions.toml'

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,11 +34,14 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.8.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.com.google.android.material)
+    implementation(libs.bundles.retrofit)
+
+    testImplementation(libs.junit)
+
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.androidx.test.espresso.core)
 }
